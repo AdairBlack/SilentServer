a71e6df11017250557f432d317bbba2b53c792de
edgar <eternaledgar@qq.com>
2024-06-20 23:12:58 +0800

    add get receive buffer size info
    Add get receive buffer info buffer.
    
    Signed-off-by: edgar <eternaledgar@qq.com>

--- Server/src/SocketServer.cpp
+++ Server/src/SocketServer.cpp
@@ -40,17 +40,16 @@ WebServer::SocketServer::SocketServer(char *ip_, int port_, int backlog_) : ip(i
     inet_pton(AF_INET, ip, &ipv4Address.sin_addr);
     ipv4Address.sin_port = htons(port);
 
-    // Set TCP receive buffer size
-    int recvBufSize = 4096;
-    int length = 0;
-    int retval = setsockopt(socketFd, SOL_SOCKET, SO_RCVBUF, &recvBufSize, sizeof(recvBufSize));
+    //get receive buffer size
+    int recvBufSize = 0;
+    socklen_t optlen = sizeof(recvBufSize);
+    int retval = getsockopt(socketFd, SOL_SOCKET, SO_RCVBUF, &recvBufSize, &optlen);
     if (-1 == retval)
     {
-        printf("Error: Set socket receive buffer size failed! errno: %d\n", errno);
+        printf("Error: Getting receive buffer size failed! errno: %d\n", errno);
         return;
     }
-    getsockopt(socketFd, SOL_SOCKET, SO_RCVBUF, &recvBufSize, (socklen_t *)&length);
-    printf("Receive buffer size: %d\n", length);
+    printf("Receive buffer size: %d\n", recvBufSize);
 
     // Bind socket fd to ip
     int ret = bind(socketFd, (struct sockaddr *)&ipv4Address, sizeof(ipv4Address));
